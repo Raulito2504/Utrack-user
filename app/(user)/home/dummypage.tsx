97360d95100d81bbbfb6430e102bf05ea61cff2f
Raul Antonio Arias Rafael <148139512+Raulito2504@users.noreply.github.com>
2025-03-17 14:20:08 -0500

    todo pa

--- app/(user)/home/dummypage.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex flex-1 items-center justify-center">
-      <Text>Dummy page</Text>
-    </View>
-  );
-}
